9741433f78d7deafb4335e6ca06375ea4d5bad67
brendanpyzik <brendanpyzik@my.ccsu.edu>
2019-11-14 15:47:31 -0500

    cool

---
+++ ArtMuseum/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.login, name='login'),
+    path('index/', views.index, name='index'),
+    path('search/', views.search, name='search'),
+    path('register/', views.register, name='register'),
+    path('artist/', views.artist, name='artist'),
+    path('piece/', views.piece, name='piece'),
+]
