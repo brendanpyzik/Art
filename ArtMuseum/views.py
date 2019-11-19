f05f0f14ab138817cd26adfe10c606ad2624c5b3
jenn-jelley <52045153+jenn-jelley@users.noreply.github.com>
2019-11-19 13:53:59 -0500

    .

--- ArtMuseum/views.py
+++ ArtMuseum/views.py
@@ -1,25 +1,31 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def login(request):
     template = "index.html"
     return render(request, template)
 
+
 def index(request):
     template = "index.html"
     return render(request, template)
+
 
 def search(request):
     template = "search.html"
     return render(request, template)
 
+
 def register(request):
     template = "register.html"
     return render(request, template)
 
+
 def artist(request):
     template = "artist.html"
     return render(request, template)
+
 
 def piece(request):
     template = "piece.html"
