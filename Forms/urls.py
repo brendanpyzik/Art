c7746c7d714669bfa9fd6d20e19823725f990e9c
Brendan <brendanpyzik@my.ccsu.edu>
2019-11-24 16:02:29 -0500

    well done

---
+++ Forms/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('addartist/', views.add_artist, name='addartist'),
+    path('addpiece/', views.add_piece, name='addpiece'),
+]
--- Forms/views.py
+++ Forms/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def add_artist(request):
+    if not request.user.is_authenticated:
+        return render(request, 'registration/login.html', {'message': None})
+    return render(request, 'registration/profile.html')
+
+def add_piece(request):
+    if not request.user.is_authenticated:
+        return render(request, 'registration/login.html', {'message': None})
+    return render(request, 'registration/profile.html')
