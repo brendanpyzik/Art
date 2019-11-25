78131fb1189870d0cab285cf455236daf77730d2
Brendan <brendanpyzik@my.ccsu.edu>
2019-11-25 18:43:21 -0500

    well done

--- Forms/views.py
+++ Forms/views.py
@@ -2,12 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def add_artist(request):
-    print("works")
+    print("worksARTIST")
     if not request.user.is_authenticated:
         return render(request, 'registration/login.html', {'message': None})
     return render(request, 'registration/profile.html')
 
 def add_piece(request):
+    print("worksPiece")
     if not request.user.is_authenticated:
         return render(request, 'registration/login.html', {'message': None})
     return render(request, 'registration/profile.html')
