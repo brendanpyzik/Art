b21f27e5a80dcb09aff80d8cca328044af2474cf
Brendan <brendanpyzik@my.ccsu.edu>
2019-11-25 18:55:45 -0500

    well done

--- Forms/views.py
+++ Forms/views.py
@@ -5,10 +5,10 @@ def add_artist(request):
     print("worksARTIST")
     if not request.user.is_authenticated:
         return render(request, 'registration/login.html', {'message': None})
-    return render(request, 'registration/profile.html')
+    return render(request, 'Forms/addinfo.html', {'message': 'Artist Added!'})
 
 def add_piece(request):
     print("worksPiece")
     if not request.user.is_authenticated:
         return render(request, 'registration/login.html', {'message': None})
-    return render(request, 'registration/profile.html')
+    return render(request, 'Forms/addinfo.html', {'message': 'Piece Added!'})
